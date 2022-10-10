9c07dee335a2ebb58b93f1c47f367ad5a807143f
AaronHtlDornbirn <aaron.haltmeier@student.htldornbirn.at>
2022-10-10 13:31:11 +0200

    Aufgabe 03 und 04 gemacht

---
+++ 03_Loops.py
@@ -0,0 +1,7 @@
+import random
+
+count = 0
+
+for i in range (0,101,1):
+    count += i
+print (count)
---
+++ 04_Loops.py
@@ -0,0 +1,8 @@
+import random
+
+count = 0
+
+for i in range (2,1001,1):
+    if i % 2 == 0:
+        count += i
+print (count)
